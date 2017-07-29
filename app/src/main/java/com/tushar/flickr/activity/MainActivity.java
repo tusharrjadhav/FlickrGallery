04a8e49901a1e30b99c1da71f0081281146d2283
Tushar Jadhav <tushar.jadhav@sogeti.nl>
2017-07-30 00:56:54 +0530

    Adding main Activity

---
+++ app/src/main/java/com/tushar/flickr/activity/MainActivity.java
@@ -0,0 +1,20 @@
+package com.tushar.flickr.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tushar.flickr.R;
+
+/**
+ * Created by Tushar Jadhav on 30/07/17
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+    }
+}
