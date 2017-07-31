6edfcf8e8d5e462bd812ae6a97f9e983433a8605
Tushar Jadhav <tushar.jadhav@sogeti.nl>
2017-07-31 12:44:12 +0530

    Creating gallery with static image

--- app/src/main/java/com/tushar/flickr/activity/MainActivity.java
+++ app/src/main/java/com/tushar/flickr/activity/MainActivity.java
@@ -1,20 +1,72 @@
 package com.tushar.flickr.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import com.tushar.flickr.R;
+import com.tushar.flickr.adapter.GalleryAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by Tushar Jadhav on 30/07/17
  */
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Drawable> images;
+    private GalleryAdapter mAdapter;
+    private RecyclerView recyclerView;
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerView = findViewById(R.id.recycler_view);
+
+        images = new ArrayList<>();
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+        images.add(ContextCompat.getDrawable(this, R.drawable.tushar));
+    }
+
+    public void onResume() {
+        super.onResume();
+        mAdapter = new GalleryAdapter(getApplicationContext(), images);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
     }
 }
---
+++ app/src/main/java/com/tushar/flickr/adapter/GalleryAdapter.java
@@ -0,0 +1,115 @@
+package com.tushar.flickr.adapter;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.tushar.flickr.R;
+
+import java.util.List;
+
+/**
+ * Created by Tushar Jadhav on 30/07/17
+ */
+
+public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.MyViewHolder> {
+
+    private List<Drawable> images;
+    private Context mContext;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public ImageView thumbnail;
+
+        public MyViewHolder(View view) {
+            super(view);
+            thumbnail = view.findViewById(R.id.thumbnail);
+        }
+    }
+
+
+    public GalleryAdapter(Context context, List<Drawable> images) {
+        mContext = context;
+        this.images = images;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.thumbnail, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Drawable image = images.get(position);
+
+        holder.thumbnail.setImageDrawable(image);
+
+        /*Glide.with(mContext).load(image)
+                .thumbnail(0.5f)
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(holder.thumbnail);*/
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    public interface ClickListener {
+        void onClick(View view, int position);
+
+        void onLongClick(View view, int position);
+    }
+
+    public static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+        private GestureDetector gestureDetector;
+        private GalleryAdapter.ClickListener clickListener;
+
+        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final GalleryAdapter.ClickListener clickListener) {
+            this.clickListener = clickListener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && clickListener != null) {
+                        clickListener.onLongClick(child, recyclerView.getChildLayoutPosition(child));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+            if (child != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
+                clickListener.onClick(child, rv.getChildLayoutPosition(child));
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
+    }
+}
